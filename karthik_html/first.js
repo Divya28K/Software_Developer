a589e5c872848d96081e80fe18f7cb58504aaddb
Divya <divyakasturi87@gmail.com>
2025-06-18 18:58:35 +0530

    Add files via upload

---
+++ karthik_html/first.js
@@ -0,0 +1,14 @@
+console.log("this is from head part")
+alert("this is a alert box")
+{
+    var a =10
+    let b =20
+        b =30
+    const c =30
+    console.log(b);
+    console.log(c);
+}
+{
+    a=50
+    console.log(a);
+}
